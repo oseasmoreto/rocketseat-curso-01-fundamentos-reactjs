068e9659aa08780444bf1316e63beac847babacf
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-19 11:01:17 -0300

    Use props in components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,12 @@
-export function Post(){
-  return <p>Post</p>
+//props: {author:sting, content:string}
+
+export function Post(props){
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+      <h4>Autor: {props.author}</h4>
+      <hr />
+    </div>
+  )
 }
